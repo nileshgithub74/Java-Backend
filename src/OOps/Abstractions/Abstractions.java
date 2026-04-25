fd0e6a7eac718224fd5e09262822232905f65961
Nilesh Kumar <kumarnilesh843127@gmail.com>
2026-04-26 00:15:35 +0530

    Inteface and Abstract methods

---
+++ src/OOps/Abstractions/Abstractions.java
@@ -0,0 +1,14 @@
+package OOps.Abstractions;
+
+
+// abstraction -  it is acheived by  abstract claases or interfacecs;
+
+
+
+public class Abstract {
+
+    public static void main(String[] args){
+        
+
+    }
+}
